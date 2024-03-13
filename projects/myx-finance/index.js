0b4cbae9d210e0e90820702b1dc556ae106de865
myxjohnny <johnny@myx.finance>
2024-03-13 14:18:15 +0800

    feat: add myx-finance adapters

---
+++ projects/myx-finance/index.js
@@ -0,0 +1,33 @@
+const { post } = require("../helper/http");
+const { toUSDTBalances } = require("../helper/balances");
+const { default: BigNumber } = require("bignumber.js");
+const { ethers } = require("ethers");
+
+async function getTvl() {
+  const { api } = arguments[3];
+  const { endpoint } = config[api.chain];
+
+  const result = await post(endpoint, {"account":ethers.ZeroAddress,"pageNum":1,"pageSize":Number.MAX_VALUE});
+
+  const tvl =  result?.data?.records?.reduce((acc, item) => {
+    return acc.plus(item?.lpTvl || 0)
+  }, new BigNumber(0))
+
+
+  return toUSDTBalances(tvl?.toFixed() || '0')
+}
+
+module.exports = {
+  doublecounted: true,
+  misrepresentedTokens: true,
+  timetravel: false,
+};
+
+const config = {
+  arbitrum: { endpoint: "https://api-arb.myx.finance/lp/mlpBuy/stat/page" },
+  linea: { endpoint: "https://api-linea.myx.finance/lp/mlpBuy/stat/page" },
+};
+
+Object.keys(config).forEach((chain) => {
+  module.exports[chain] = { tvl: getTvl };
+});
